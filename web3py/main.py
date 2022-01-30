c232cc74290d1f25358419e79aa6b03fe7bfcc22
Davide <davide.pruscini@studio.unibo.it>
2022-01-30 18:36:32 +0100

    Add CloudSLA creation and activation
    Some tests

--- web3py/main.py
+++ web3py/main.py
@@ -6,18 +6,22 @@ from web3.middleware import geth_poa_middleware
 '''
 # HTTPProvider:
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
-
 # Convenience method to automatically detect the provider
 from web3.auto import w3
 print(f'Auto detect: {w3.isConnected()}')
 '''
 
 blockchain_address = 'ws://127.0.0.1:8546'
+accounts = [
+    '0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73',
+    '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
+    '0xf17f52151EbEF6C7334FAD080c5704D77216b732'
+]
 
-# Local providers
+# Connect to blockchain, WebSocket
 w3 = Web3(Web3.WebsocketProvider(blockchain_address))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-w3.eth.defaultAccount = w3.eth.accounts[0]
+w3.eth.defaultAccount = accounts[0]
 
 compiled_factory_path = '../build/contracts/Factory.json'
 deployed_factory_address = '0x6486A01e45648B1aDCc51D375Af3a7c0a5e9002a'
@@ -33,6 +37,7 @@ with open(compiled_oracle_path) as file:
     contract_oracle_json = json.load(file)
     contract_oracle_abi = contract_oracle_json['abi']
 
+# CloudSLA creation and activation
 contract_factory = w3.eth.contract(address=deployed_factory_address, abi=contract_factory_abi)
 print(f"All Factory functions: {contract_factory.all_functions()}")
 
@@ -41,13 +46,25 @@ print(f"All FileDigestOracle functions: {contract_oracle.all_functions()}")
 
 price = Web3.toWei(5, 'ether')
 test_validity_duration = 60 ** 2
-# print(contract_factory.functions.createChild(deployed_oracle_address, ))
+res_create_child = contract_factory.functions.createChild(
+    deployed_oracle_address,
+    accounts[1],
+    price,
+    test_validity_duration,
+    1,
+    1 # msg.sender?
+).call()
+print(f'Call createChild: {res_create_child}')
+
+res_sm_address = contract_factory.functions.getSmartContractAddress(
+    accounts[1]
+).call()
+print(f'Call getSmartContractAddress: {res_sm_address}')
 
 '''
 print(f"Block number: {w3.eth.block_number}")
 last_block = json.loads(Web3.toJSON(w3.eth.get_block('latest')))
 print(f'Last block: {json.dumps(last_block, indent=4, sort_keys=True)}')
-
 balance = w3.eth.get_balance('0x627306090abaB3A6e1400e9345bC60c78a8BEf57')
 print(f'Balance (wei): {balance}')
 print(f"Balance (eth): {Web3.fromWei(balance, 'ether')}")
