29fdda3f55c76eb5ac1618bde5b1f43d946ae3db
Davide <davide.pruscini@studio.unibo.it>
2022-02-05 18:36:55 +0100

    All functions test done

--- web3py/main.py
+++ web3py/main.py
@@ -1,19 +1,21 @@
 import json
-import os
-from time import time
 
 import argparse
 
+from eth_typing import Address
 from web3 import Web3
 from web3.auto import w3
+from web3.contract import Contract
 from web3.middleware import geth_poa_middleware
 
 from settings import (
     COMPILED_FACTORY_PATH,
     COMPILED_ORACLE_PATH,
     COMPILED_CLOUD_SLA_PATH,
-    DEBUG, WEB_SOCKET_URI
+    WEB_SOCKET_URI
 )
+
+cloud_sla_address = Address(b'0x0')
 
 
 def get_addresses(blockchain: str) -> tuple:
@@ -40,43 +42,37 @@ def get_settings(blockchain: str) -> tuple:
     return quorum_accounts, quorum_private_keys
 
 
-def get_contract_abi(compiled_contract_path: str) -> list:
-    if DEBUG:
-        print(f'-- Get contract abi for {os.path.basename(compiled_contract_path)} --')
-    with open(compiled_contract_path) as file:
-        contract_json = json.load(file)
-        contract_abi = contract_json['abi']
-    return contract_abi
+def get_contract(address: Address, compiled_contract_path: str) -> Contract:
+    def get_abi(path: str) -> list:
+        with open(path) as file:
+            contract_json = json.load(file)
+            contract_abi = contract_json['abi']
+        return contract_abi
+
+    abi = get_abi(compiled_contract_path)
+    contract = w3.eth.contract(address=address, abi=abi)
+
+    return contract
 
 
-def sign_send_transaction(tx: dict, pk: str, label: str):
-    if DEBUG:
-        print('-- Start sign transaction --')
-        print(f'{label}: {json.dumps(tx, indent=4, sort_keys=True)}')
-
+def sign_send_transaction(tx: dict, pk: str):
     signed_tx = w3.eth.account.sign_transaction(tx, private_key=pk)
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
-    if DEBUG:
-        tx_receipt_json = json.loads(Web3.toJSON(tx_receipt))
-        print(f'{label}_receipt: {json.dumps(tx_receipt_json, indent=4, sort_keys=True)}')
-        print('-- End sign transaction --')
     return tx_receipt
 
-def print_transaction_receipt(tx_receipt:dict, label:str):
-    tx_receipt_json = json.loads(Web3.toJSON(tx_receipt))
-    print(f'{label}_receipt: {json.dumps(tx_receipt_json, indent=4, sort_keys=True)}')
 
 def cloud_sla_creation_activation():
-    factory_abi = get_contract_abi(COMPILED_FACTORY_PATH)
-    oracle_abi = get_contract_abi(COMPILED_ORACLE_PATH)
-
-    contract_factory = w3.eth.contract(address=FACTORY_ADDRESS, abi=factory_abi)
-    contract_oracle = w3.eth.contract(address=ORACLE_ADDRESS, abi=oracle_abi)
-
+    # Parameters
     price = Web3.toWei(5, 'ether')
     test_validity_duration = 60 ** 2
+
+    # Contracts
+    contract_factory = get_contract(FACTORY_ADDRESS, COMPILED_FACTORY_PATH)
+    contract_oracle = get_contract(ORACLE_ADDRESS, COMPILED_ORACLE_PATH)
+
+    # Transactions
     tx_create_child = contract_factory.functions.createChild(
         contract_oracle.address,
         accounts[1],
@@ -89,135 +85,369 @@ def cloud_sla_creation_activation():
         'from': accounts[0],
         'nonce': w3.eth.get_transaction_count(accounts[0])
     })
-    sign_send_transaction(tx_create_child, private_keys[0], label='create_child')
+    sign_send_transaction(tx_create_child, private_keys[0])
 
     tx_sm_address = contract_factory.functions.getSmartContractAddress(
         accounts[1]
     ).call()
 
-    cloud_sla_abi = get_contract_abi(COMPILED_CLOUD_SLA_PATH)
-    contract_cloud_sla = w3.eth.contract(address=tx_sm_address, abi=cloud_sla_abi)
+    # Contract
+    contract_cloud_sla = get_contract(tx_sm_address, COMPILED_CLOUD_SLA_PATH)
 
+    # Transaction
     tx_deposit = contract_cloud_sla.functions.Deposit().buildTransaction({
         'gasPrice': 0,
         'from': accounts[1],
         'nonce': w3.eth.get_transaction_count(accounts[1]),
         'value': price
     })
-    sign_send_transaction(tx_deposit, private_keys[1], label='deposit')
+    sign_send_transaction(tx_deposit, private_keys[1])
 
+    print('CloudSLA creation and activation: OK')
+    print(f'\taddress: {tx_sm_address}')
     return tx_sm_address
 
 
-def upload(cloud_sla_address):
+def upload():
+    # Parameters
+    filepath = 'test.pdf'
     hash_digest = '0x9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
 
-    cloud_sla_abi = get_contract_abi(COMPILED_CLOUD_SLA_PATH)
-    contract_cloud_sla = w3.eth.contract(address=cloud_sla_address, abi=cloud_sla_abi)
+    # Contract
+    contract_cloud_sla = get_contract(cloud_sla_address, COMPILED_CLOUD_SLA_PATH)
 
-    challenge = Web3.keccak(
-        hexstr=contract_cloud_sla.encodeABI(fn_name='UploadRequest', args=['test.pdf', hash_digest])
+    # Transactions
+    challenge = Web3.solidityKeccak(
+        ['bytes32'], [hash_digest]
     )
 
     tx_upload_request = contract_cloud_sla.functions.UploadRequest(
-        'test.pdf',
+        filepath,
         challenge
     ).buildTransaction({
         'gasPrice': 0,
         'from': accounts[1],
         'nonce': w3.eth.get_transaction_count(accounts[1])
     })
-    sign_send_transaction(tx_upload_request, private_keys[1], 'upload_request')
+    sign_send_transaction(tx_upload_request, private_keys[1])
 
     tx_upload_request_ack = contract_cloud_sla.functions.UploadRequestAck(
-        'test.pdf'
+        filepath
     ).buildTransaction({
         'gasPrice': 0,
         'from': accounts[0],
         'nonce': w3.eth.get_transaction_count(accounts[0])
     })
-    sign_send_transaction(tx_upload_request_ack, private_keys[0], 'upload_request_ack')
+    sign_send_transaction(tx_upload_request_ack, private_keys[0])
 
     tx_upload_transfer_ack = contract_cloud_sla.functions.UploadTransferAck(
-        'test.pdf',
+        filepath,
         hash_digest
     ).buildTransaction({
         'gasPrice': 0,
         'from': accounts[0],
         'nonce': w3.eth.get_transaction_count(accounts[0])
     })
-    sign_send_transaction(tx_upload_transfer_ack, private_keys[0], 'upload_transfer_ack')
+    sign_send_transaction(tx_upload_transfer_ack, private_keys[0])
+
+    print('Upload: OK')
 
 
-def read(cloud_sla_address):
-    cloud_sla_abi = get_contract_abi(COMPILED_CLOUD_SLA_PATH)
-    contract_cloud_sla = w3.eth.contract(address=cloud_sla_address, abi=cloud_sla_abi)
+def read():
+    # Parameters
+    filepath = 'test.pdf'
+    url = 'www.test.com'
 
+    # Contract
+    contract_cloud_sla = get_contract(cloud_sla_address, COMPILED_CLOUD_SLA_PATH)
+
+    # Transactions
     tx_read_request = contract_cloud_sla.functions.ReadRequest(
-        'test.pdf'
+        filepath
     ).buildTransaction({
         'gasPrice': 0,
-        'from':accounts[1],
+        'from': accounts[1],
         'nonce': w3.eth.get_transaction_count(accounts[1])
     })
-    recepit_read=sign_send_transaction(tx_read_request, private_keys[1], label='read_request')
+    sign_send_transaction(tx_read_request, private_keys[1])
 
     tx_read_request_ack = contract_cloud_sla.functions.ReadRequestAck(
-        'test.pdf',
-        'www.test.com'
+        filepath,
+        url
     ).buildTransaction({
         'gasPrice': 0,
-        'from':accounts[0],
+        'from': accounts[0],
         'nonce': w3.eth.get_transaction_count(accounts[0])
     })
-    recepit_read_ack=sign_send_transaction(tx_read_request_ack, private_keys[0], label='read_request_ack')
-    #print_transaction_receipt(recepit_read,"read")
-    #print_transaction_receipt(recepit_read_ack,"read_ack")
+    sign_send_transaction(tx_read_request_ack, private_keys[0])
+
+    print('Read: OK')
 
 
-def delete(cloud_sla_address):
-    cloud_sla_abi = get_contract_abi(COMPILED_CLOUD_SLA_PATH)
-    contract_cloud_sla = w3.eth.contract(address=cloud_sla_address, abi=cloud_sla_abi)
+def delete():
+    # Parameter
+    filepath = 'test.pdf'
 
+    # Contract
+    contract_cloud_sla = get_contract(cloud_sla_address, COMPILED_CLOUD_SLA_PATH)
+
+    # Transactions
     tx_delete_request = contract_cloud_sla.functions.DeleteRequest(
-        'test.pdf'
+        filepath
     ).buildTransaction({
         'gasPrice': 0,
-        'from':accounts[1],
+        'from': accounts[1],
         'nonce': w3.eth.get_transaction_count(accounts[1])
     })
-    sign_send_transaction(tx_delete_request, private_keys[1], label='delete_request')
+    sign_send_transaction(tx_delete_request, private_keys[1])
 
     tx_delete = contract_cloud_sla.functions.Delete(
-        'test.pdf',
+        filepath
     ).buildTransaction({
         'gasPrice': 0,
-        'from':accounts[0],
+        'from': accounts[0],
         'nonce': w3.eth.get_transaction_count(accounts[0])
     })
-    sign_send_transaction(tx_delete, private_keys[0], label='delete')
-    #print_transaction_receipt(recepit_read,"read")
-    #print_transaction_receipt(recepit_read_ack,"read_ack")
-    
+    sign_send_transaction(tx_delete, private_keys[0])
+
+    print('Delete: OK')
+
+
+def file_check_undeleted_file():
+    # Parameters
+    filepath = 'test.pdf'
+    url = 'www.test.com'
+    hash_digest = '0x9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
+
+    # Contracts
+    contract_cloud_sla = get_contract(cloud_sla_address, COMPILED_CLOUD_SLA_PATH)
+    contract_oracle = get_contract(ORACLE_ADDRESS, COMPILED_ORACLE_PATH)
+
+    # Transactions
+    tx_file_hash_request = contract_cloud_sla.functions.FileHashRequest(
+        filepath
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[1],
+        'nonce': w3.eth.get_transaction_count(accounts[1])
+    })
+    sign_send_transaction(tx_file_hash_request, private_keys[1])
+
+    tx_digit_store = contract_oracle.functions.DigestStore(
+        url,
+        hash_digest
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[2],
+        'nonce': w3.eth.get_transaction_count(accounts[2])
+    })
+    sign_send_transaction(tx_digit_store, private_keys[2])
+
+    tx_file_check = contract_cloud_sla.functions.FileCheck(
+        filepath
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[1],
+        'nonce': w3.eth.get_transaction_count(accounts[1])
+    })
+    sign_send_transaction(tx_file_check, private_keys[1])
+
+    print('File check for undeleted file: OK')
+
+
+def another_file_upload():
+    # Parameters
+    filepath = 'test2.pdf'
+    hash_digest = '0x1f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
+
+    # Contract
+    contract_cloud_sla = get_contract(cloud_sla_address, COMPILED_CLOUD_SLA_PATH)
+
+    # Transactions
+    challenge = Web3.solidityKeccak(
+        ['bytes32'], [hash_digest]
+    )
+
+    tx_upload_request = contract_cloud_sla.functions.UploadRequest(
+        filepath,
+        challenge
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[1],
+        'nonce': w3.eth.get_transaction_count(accounts[1])
+    })
+    sign_send_transaction(tx_upload_request, private_keys[1])
+
+    tx_upload_request_ack = contract_cloud_sla.functions.UploadRequestAck(
+        filepath
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[0],
+        'nonce': w3.eth.get_transaction_count(accounts[0])
+    })
+    sign_send_transaction(tx_upload_request_ack, private_keys[0])
+
+    tx_upload_transfer_ack = contract_cloud_sla.functions.UploadTransferAck(
+        filepath,
+        hash_digest
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[0],
+        'nonce': w3.eth.get_transaction_count(accounts[0])
+    })
+    sign_send_transaction(tx_upload_transfer_ack, private_keys[0])
+
+    print('Another file upload: OK')
+
+
+def read_deny_lost_file_check():
+    # Parameter
+    filepath = 'test2.pdf'
+
+    # Contract
+    contract_cloud_sla = get_contract(cloud_sla_address, COMPILED_CLOUD_SLA_PATH)
+
+    # Transactions
+    tx_read_request = contract_cloud_sla.functions.ReadRequest(
+        filepath
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[1],
+        'nonce': w3.eth.get_transaction_count(accounts[1])
+    })
+    sign_send_transaction(tx_read_request, private_keys[1])
+
+    tx_read_request_deny = contract_cloud_sla.functions.ReadRequestDeny(
+        filepath
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[0],
+        'nonce': w3.eth.get_transaction_count(accounts[0])
+    })
+    sign_send_transaction(tx_read_request_deny, private_keys[0])
+
+    print('Read Deny with lost file check: OK')
+
+
+def another_file_upload_read():
+    # Parameters
+    filepath = 'test3.pdf'
+    url = 'www.test3.com'
+    hash_digest = '0x2f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
+
+    # Contract
+    contract_cloud_sla = get_contract(cloud_sla_address, COMPILED_CLOUD_SLA_PATH)
+
+    # Transactions
+    challenge = Web3.solidityKeccak(
+        ['bytes32'], [hash_digest]
+    )
+
+    tx_upload_request = contract_cloud_sla.functions.UploadRequest(
+        filepath,
+        challenge
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[1],
+        'nonce': w3.eth.get_transaction_count(accounts[1])
+    })
+    sign_send_transaction(tx_upload_request, private_keys[1])
+
+    tx_upload_request_ack = contract_cloud_sla.functions.UploadRequestAck(
+        filepath
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[0],
+        'nonce': w3.eth.get_transaction_count(accounts[0])
+    })
+    sign_send_transaction(tx_upload_request_ack, private_keys[0])
+
+    tx_upload_transfer_ack = contract_cloud_sla.functions.UploadTransferAck(
+        filepath,
+        hash_digest
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[0],
+        'nonce': w3.eth.get_transaction_count(accounts[0])
+    })
+    sign_send_transaction(tx_upload_transfer_ack, private_keys[0])
+
+    tx_read_request = contract_cloud_sla.functions.ReadRequest(
+        filepath
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[1],
+        'nonce': w3.eth.get_transaction_count(accounts[1])
+    })
+    sign_send_transaction(tx_read_request, private_keys[1])
+
+    tx_read_request_ack = contract_cloud_sla.functions.ReadRequestAck(
+        filepath,
+        url
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[0],
+        'nonce': w3.eth.get_transaction_count(accounts[0])
+    })
+    sign_send_transaction(tx_read_request_ack, private_keys[0])
+
+    print('Another file upload + read: OK')
+
+
+def corrupted_file_check():
+    # Parameters
+    filepath = 'test3.pdf'
+    url = 'www.test3.com'
+    hash_digest = '0x4f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
+
+    # Contracts
+    contract_cloud_sla = get_contract(cloud_sla_address, COMPILED_CLOUD_SLA_PATH)
+    contract_oracle = get_contract(ORACLE_ADDRESS, COMPILED_ORACLE_PATH)
+
+    # Transactions
+    tx_file_hash_request = contract_cloud_sla.functions.FileHashRequest(
+        filepath
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[1],
+        'nonce': w3.eth.get_transaction_count(accounts[1])
+    })
+    sign_send_transaction(tx_file_hash_request, private_keys[1])
+
+    tx_digit_store = contract_oracle.functions.DigestStore(
+        url,
+        hash_digest
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[2],
+        'nonce': w3.eth.get_transaction_count(accounts[2])
+    })
+    sign_send_transaction(tx_digit_store, private_keys[2])
+
+    tx_file_check = contract_cloud_sla.functions.FileCheck(
+        filepath
+    ).buildTransaction({
+        'gasPrice': 0,
+        'from': accounts[1],
+        'nonce': w3.eth.get_transaction_count(accounts[1])
+    })
+    sign_send_transaction(tx_file_check, private_keys[1])
+
+    print('File Check for corrupted file: OK')
 
 
 def main():
-    '''
-    durations = []
-    for _ in range(10):
-        start = time()
-        cloud_sla_creation_activation()
-        end = time()
-        durations.append(end - start)
+    global cloud_sla_address
 
-    print(durations)
-    print(f'avg_durations: {round(sum(durations) / len(durations), 2)}')
-    '''
     cloud_sla_address = cloud_sla_creation_activation()
-    print(f'cloud_sla_address: {cloud_sla_address}')
-    upload(cloud_sla_address)
-    read(cloud_sla_address)
-    delete(cloud_sla_address)
+    upload()
+    read()
+    delete()
+    file_check_undeleted_file()
+    another_file_upload()
+    read_deny_lost_file_check()
+    another_file_upload_read()
+    corrupted_file_check()
 
 
 if __name__ == '__main__':
@@ -237,6 +467,7 @@ if __name__ == '__main__':
 
     if not w3.isConnected():
         print(f'-- Connection error --')
+        # TODO connection based on blockchain
         w3 = Web3(Web3.WebsocketProvider(WEB_SOCKET_URI))
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     print(f'-- Connection established --')
