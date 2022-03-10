1b4420f214f6ed54e2c6db2c98c15cd4e1e42191
Davide <davide.pruscini@studio.unibo.it>
2022-03-10 17:02:31 +0100

    Update number of contracts

--- web3py/run_simulation.sh
+++ web3py/run_simulation.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 . <(curl -sLo- "https://git.io/progressbar")
-SIMULATION_TIME=60
-repetition=10
-blockchain=polygon
+SIMULATION_TIME=200
+repetition=15
+blockchain=besu
+experiment=besu_qbft_4
 echo -n -e "Insert the simulation time\n"
 read SIMULATION_TIME
 echo -n -e "Insert the number of repetition\n"
 read repetition
 echo -n -e "Insert the blockchain\n"
 read blockchain
+echo -n -e "Insert the experiment\n"
+read experiment
 declare -a lambda=(2 1 0.5)
 declare -a functions=(
-  #'read'
-  'upload'
-  #'delete'
-  #'another_file_upload'
-  #'another_file_upload_read'
-  #'file_check_undeleted_file'
-  #'read_deny_lost_file_check'
   #'cloud_sla_creation_activation'
+  'upload'
+  #'read'
+  #'delete'
+  #'file_check_undeleted_file'
+  #'another_file_upload'
+  #'read_deny_lost_file_check'
+  #'another_file_upload_read'
   #'corrupted_file_check'
 )
 
-((Max = 3 * 1 * $repetition))
+NUM_LAMBDA=${#lambda[@]}
+NUM_FUNCTION=${#functions[@]}
+
+((Max = $NUM_LAMBDA * $NUM_FUNCTION * $repetition))
 counter=0
 bar::start
-for ((idx_lambda = 0; idx_lambda < 3; idx_lambda++)); do
+for ((idx_lambda = 0; idx_lambda < $NUM_LAMBDA; idx_lambda++)); do
 
   echo -e "\n\nStart simulation with lambda: ${lambda[$idx_lambda]}\n"
-  for ((m = 0; m <= 1; m++)); do
+  for ((m = 0; m < $NUM_FUNCTION; m++)); do
 
     echo -ne " - ${functions[$m]} start                  \r"
     for ((r = 0; r < ${repetition}; r++)); do
       ((counter = counter + 1))
       bar::status_changed $counter $Max
-      python main.py ${blockchain} ${functions[$m]} -l ${lambda[$idx_lambda]} -t ${SIMULATION_TIME} -s -n ${r} -e besu_qbft_4 &>/dev/null
+      python main.py ${blockchain} ${functions[$m]} -l ${lambda[$idx_lambda]} -t ${SIMULATION_TIME} -s -n ${r} -e ${experiment} &>/dev/null
     done
     echo -ne " - ${functions[$m]} done                   \r"
     echo -e "\n"
